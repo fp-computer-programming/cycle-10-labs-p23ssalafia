800be35726d41069c837adb70c0cea2da3121bf5
Sean Salafia <p23ssalafia@fairfieldprep.org>
2023-01-20 13:44:09 -0500

    10-1 and 10-2 WIP

---
+++ Lab_10-1.py
@@ -0,0 +1,14 @@
+#Author: Sean Salafia 1/20/23
+
+
+sum = 0
+
+user_input =("Input number here: ")
+int(user_input)
+
+if user_input == -1:
+    print (sum)
+
+while user_input != -1:
+    sum += user_input
+    break
---
+++ Lab_10-2.py
@@ -0,0 +1,8 @@
+#Author: Sean Salafia 1/20/23
+
+
+sum = 0
+user_input = print(input("Input a number here: "))
+while int(user_input) <= 1:
+    sum += int(user_input)
+    print (sum)
