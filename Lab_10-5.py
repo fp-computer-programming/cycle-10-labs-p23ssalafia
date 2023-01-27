c6d676f003a7bb0e4898dbea8511dc881912a06f
Sean Salafia <p23ssalafia@fairfieldprep.org>
2023-01-27 12:24:19 -0500

    10-5 WIP

--- Lab_10-5.py
+++ Lab_10-5.py
@@ -5,22 +5,23 @@ def add_foods(food):
     six_letter_foods = []
     not_foods = []
     short_foods = []
-    food_index = []
-    try:
-        for index,variables in enumerate:
-            foodx  = [*variables]
-            six_letter_foods.append(foodx[7])
-    except TypeError:
-        not_foods.append(food[food_index])
-    except IndexError:
-        short_foods.append(food[food_index])
-
-    six_letter_foods = 'Six Letter Foods: ', 
-    not_foods = 'Not Foods: ', 
-    short_foods = 'Short Foods: ',
+    for index,variables in enumerate:
+        try:
+            food_string = str(variables)
+            if len(food_string) >= 8:
+                six_letter_foods.append(food_string[7])
+            else:
+                short_foods.append(food_string)
+        except TypeError:
+            not_foods.append(food[food_string])
+        except IndexError:
+            short_foods.append(food[food_string])
 
     return(six_letter_foods,not_foods,short_foods)
 
+six_letter_foods = 'Six Letter Foods: ', 
+not_foods = 'Not Foods: ', 
+short_foods = 'Short Foods: ',
 
 add_foods(['potoatoes','salsa','cherries','banana','apple'])
 add_foods(['naan','celery','buckwheat',7,'clementine'])
